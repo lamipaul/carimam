dfbcb57506f0fee85c94647d5bd945f5d8c60348
lamipaul <paulobest25@gmail.com>
2021-05-07 12:02:46 +0200

    minor fixes

--- sort_cluster.py
+++ sort_cluster.py
@@ -7,9 +7,9 @@ import umap
 import hdbscan
 
 
-folder = '../DATA/BONAIRE/session1_20201217to20210126/' # path to a given recording station folder
-# list of spectrogram configs to be clustered
-configs = [
+folder = '../../DATA/BONAIRE/session1_20201217to20210126/' # path to a given recording station folder
+outfolder = '../pngs/' # path to the folder to print clustered pngs
+configs = [ # list of spectrogram configs to be clustered
     {'id':'mel_BBF', 'fs': 2000},
     {'id':'mel_HBF', 'fs': 16000},
     {'id':'mel_BMF', 'fs': 64000},
@@ -17,76 +17,74 @@ configs = [
     {'id':'stft_HMF', 'fs': 64000},
     {'id':'stft_HF', 'fs': 256000}
 ]
-
 sort = True # whether we sort each frequency bins by descending order (used to build an energy distribution image and eliminate time dependent features)
+fs = 512000 # TODO adapt to each recording station (some run at 256kHz)
+chunksize = 20 # size (in time bins) of chunks of spectrogram to cluster independantly
 
 #get filenames list, filter _spec.npy files only (possibly sample a subset randomly for testing)
 fns = pd.Series(os.listdir(folder))
 fns = fns[fns.str.endswith('_spec.npy')] #.sample(500)
 
-fs = 512000 # TODO adapt to each recording station (some run at 256kHz)
-chunksize = 20 # size (in time bins) of chunks of spectrogram to cluster independantly
-
 # for each configuration, we load spectrograms, project features, cluster, and plot spectrograms in pngs
 for c in configs:
     # arrays X and meta will hold features and metadata for each samples to be projected / clustered
     X, meta = [], []
-    for f in tqdm(fns, desc='loading specs for '+c['id'], leave=False):
+    for f in tqdm(fns, desc='loading spectros for '+c['id'], leave=False):
         # load the spectrogram from the .npy file
         spectro = np.load(folder+f, allow_pickle=True).item()[c['id']]
-        
+
         # cut the spectrogram in chunks time wise
-        for offset in np.arange(0, spectro.shape[1]-chunksize, chunksize): 
+        for offset in np.arange(0, spectro.shape[1]-chunksize, chunksize):
             # get logarithmic magnitude
-            temp = np.log10(spectroDict[:,offset:offset+chunksize])
+            temp = np.log10(spectro[:,offset:offset+chunksize])
             if sort:
                 # if sort is True, we sort each frequency bin in descending order
                 temp = np.flip(np.sort(temp, axis=1), axis=1)
                 # we can then select a subset of bins (similar to quantiles)
-                X.append(temp[:, [1, 3, 5]]) 
-            else : 
+                X.append(temp[:, [1, 3, 5]])
+            else :
                 # else, we use the whole spectrogram as input features for projection
                 X.append(temp)
             # save filename and offset to retrieve the sample later on
             meta.append({'fn':f, 'offset':offset})
-    
+
     # rearange X and meta arrays for easier later use
     X = np.array(X).reshape(len(X), -1)
     meta = pd.DataFrame().from_dict(meta)
-    
+
     # embed the features using UMAP
     project = umap.UMAP()
     embed = project.fit_transform(X)
-    
+
     # cluster the embedings (min_cluster_size and min_samples parameters need to be tuned)
     clusterer = hdbscan.HDBSCAN(min_cluster_size=100, min_samples=10).fit(embed) # au depart 50 et 3
     meta['cluster'] = clusterer.labels_
-    
+
     # display information for the user to check whether the clustering has gone well or not
     print('clusters for '+c['id'])
     print(meta.groupby('cluster').agg({'fn':'nunique', 'offset':'count'})\
-          .rename(columns={'fn':'Number of files in cluster', 'offset':'Number of samples in cluster'}))
+          .rename(columns={'fn':'n unique files', 'offset':'n samples'}))
     plt.scatter(embed[:,0], embed[:,1], c=clusterer.labels_ , cmap="Paired", s=1)
     plt.show()
     plt.savefig('scatter_'+c['id'])
     plt.close()
-    
+
     # For each cluster, we create a folder of png spectrograms
     for cluster, grp in meta.groupby('cluster'):
         # if the cluster is more than 300 samples big, we consider it is noise (to tune depending on dataset size)
-        if len(grp) > 300: 
+        if len(grp) > 300:
             continue
-        
+
         # create the cluster folder in the config folder
-        os.system('mkdir -p pngs/'+c['id']+'/'+str(cluster))
-        
+        os.system('mkdir -p '+outfolder+c['id']+'/'+str(cluster))
+
         # for each sample in the cluster, we plot the spectrogram for quick visualisation of the clustering result
-        for row in tqdm(grp.itertuples(), desc=str(cluster), total=len(grp), leave=False):
-            
+        for row in tqdm(grp.itertuples(), desc='ploting spectros for cluster '+str(cluster), total=len(grp), leave=False):
+
             # load the spectrogram from the .npy file
             spectro = np.load(folder+row.fn, allow_pickle=True).item()[c['id']]
             plt.imshow(np.log10(spectro[:, row.offset : row.offset+chunksize]), origin='lower', aspect='auto')
-            
+
             # TODO convert the offset (in max pooled time bins) to seconds
-            plt.savefig('pngs/'+c['id']+'/'+str(cluster)+'/'+r.fn[:-9]+'_'+str(r.offset))
+            plt.savefig(outfolder+c['id']+'/'+str(cluster)+'/'+row.fn[:-9]+'_'+str(row.offset))
             plt.close()
--- spectral_extraction.py
+++ spectral_extraction.py
@@ -11,7 +11,7 @@ This file computes several spectrograms with a given set of paramters.
 Spectrograms of size 128x128 will be saved in .npy files for each found sounfiles found in a given folder
 """
 
-folder = '../DATA/BONAIRE/session1_20201217to20210126/' # path to a given recording station folder
+folder = '../../DATA/BONAIRE/session1_20201217to20210126/' # path to a given recording station folder
 winsize = 1024 # global STFT window size (we change the sample rate to tune the freq / time resolutions)
 source_fs = 512000 # TODO adapt to each session (some run at 256kHz)
 
@@ -28,24 +28,24 @@ def create_mel_filterbank(sample_rate, frame_len, num_bands, min_freq, max_freq)
     # array of triangular filters' peaks (logarithmic scale)
     peaks_hz = 700 * (np.expm1(peaks_mel / 1127))
     peaks_bin = peaks_hz * frame_len / sample_rate
-    
+
     # spectrogram freq bins indices
     x = np.arange(frame_len//2 + 1).reshape(-1, 1)
-    
+
     # build triangular filters from left and right boundaries
     l, c, r = peaks_bin[0:-2], peaks_bin[1:-1], peaks_bin[2:]
     tri_left = (x - l) / (c - l)
     tri_right = (x - r) / (c - r)
     tri = np.minimum(tri_left, tri_right)
-    
+
     # clip at zero, and normalize each filter by its area
     filterbank = np.maximum(tri, 0)
     filterbank /= filterbank.sum(axis=0)
-    
+
     # return a weight transformation matrix of shape (num_bands, frame_len//2 + 1)
     return filterbank.T
 
-""" 
+"""
 Set of spectrogram configurations (sampling rate, wether we yield a regular or mel spectrogram, mel start frequency), 
 each tuned to focus on a given type of vocalisations. e.g. BBF configuration samples at 2kHz. 
 The output mel spectrogram will thus range from 0 to 1kHz, with 512 logarithmic spaced freq bins.
@@ -67,37 +67,37 @@ for c in configs:
 print('doing ', folder)
 #get filenames list, filter wav files only (possibly sample a subset randomly for testing)
 fns = pd.Series(os.listdir(folder))
-fns = fns[fns.str.endswith('WAV')] #.sample(500)
+fns = fns[fns.str.endswith('WAV')].sample(500)
 
-# for each sound file 
+# for each sound file
 for fn in tqdm(fns):
     try:
         sig, fs = sf.read(folder+fn)
     except:
         print('failed with ',fn)
         continue
-    
+
     # we build a dictionnary containing a spectrogram for each configuration
     out = {}
     for c in configs:
-        
+
         # low pass filter at next nyquist frequency and undersample the signal
-        csig = signal.sosfiltfilt(c['sos'], sig) 
+        csig = signal.sosfiltfilt(c['sos'], sig)
         csig = csig[::(fs//c['fs'])]
-        
+
         # compute the magnitude spectrogram using the STFT
-        if c['id'] != 'HF': 
+        if c['id'] != 'HF':
             f, t, spec = signal.stft(csig, fs=c['fs'], nperseg=winsize, noverlap=winsize//2)
         else: # special winsize for HF
             f, t, spec = signal.stft(csig, fs=c['fs'], nperseg=256, noverlap=128)
         spec = np.abs(spec)
-        
+
         # we undersample the spectrogram over the time dimension to get 128 time bins only
         time_uds = spec.shape[1]//128
         if c['nomel']:
             out['stft_'+c['id']] = maximum_filter(spec, (1, time_uds))[:128, ::time_uds]
         if c['mel']:
             out['mel_'+c['id']] = maximum_filter(np.matmul(c['melbank'], spec), (1, time_uds))[:,::time_uds]
-            
+
     # save the dictionnary of spectrograms with at the soundfile location, with the input filename + '_spec.npy'
     np.save(folder+fn.rsplit('.', 1)[0]+'_spec.npy', out)
